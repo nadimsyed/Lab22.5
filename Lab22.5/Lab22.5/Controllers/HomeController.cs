738b35213c20ea8dda626e78662b69a01f4a088d
Nadim <syed.m.nadim@gmail.com>
2018-05-23 15:37:10 -0400

    Set up the Index in Home, the LogIn in Home, their associated views as well as the form that is storing the information. Store the information into 3 seperate sessions.

---
+++ Lab22.5/Lab22.5/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab22._5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string userName, string color, string favoriteFood)
+        {
+            ViewBag.UserName = userName;
+            ViewBag.Color = color;
+            ViewBag.FavoriteFood = favoriteFood;
+
+            return View();
+        }
+
+        public ActionResult LogIn(string userName, string color, string favoriteFood)
+        {
+            if (Session["UserName"] == null)
+            {
+                Session.Add("UserName", userName);
+            }
+            if (Session["Color"] == null)
+            {
+                Session.Add("Color", color);
+            }
+            if (Session["FavoriteFood"] == null)
+            {
+                Session.Add("FavoriteFood", favoriteFood);
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
